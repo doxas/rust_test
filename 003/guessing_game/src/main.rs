59ac99cc18f03d0ca6d5bd8ea837628a9b9c581d
sugimoto-m <masahiro-s@digirea.com>
2018-01-05 12:42:13 +0900

    [wip] add rang crate

--- 003/guessing_game/src/main.rs
+++ 003/guessing_game/src/main.rs
@@ -1,9 +1,16 @@
 
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main(){
     println!("Guess tha number!");
     println!("Please input your guess.");
+
+    let secret = rand::thread_rng().gen_range(0, 101);
+
+    println!("The secret number is: {}", secret);
 
     let mut guess = String::new();
 
@@ -26,3 +33,16 @@ fn main(){
 // Result の中身が正しくない時、expect は引数に受け取った文字列と共に panic! 、つまり意図的に落ちる。
 //
 // println の中でプレースホルダ {} を使うと、そこに続く引数を与えて出力が行える。
+//
+// ----------------------------------------------------------------------------
+//
+// クレート（crate）は、いわゆるモジュールで Cargl.toml の dependencies にバージョンと共に記載する
+// ことでバンドルされ、extern によってインポートされる。
+// クレートが npm で言うところのパッケージであり、Create.io というパブリックなドメインがある。
+//
+// use rand::Rng は一見不要に思えるが、これはトレイトと呼ばれる型定義のようなものをインポートするた
+// めに必要な記述。Rng というトレイト（定義）を参照するために、これを行う必要がある。
+//
+// rand::thread_rng() は現在のスレッドに乱数生成器をインスタンス化する。
+// インスタンスは Rng トレイトがスコープに入っているため gen_range をコールすることができる。
+// このメソッドは引数をふたつ持ち、第一引数に下限を、第二引数に上限を取る。（以上、未満）
