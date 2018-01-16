6b8e1ce200906ce9e58eef9d402d26995264238b
doxas <eizanco@gmail.com>
2018-01-17 02:43:21 +0900

    add new sample

---
+++ 012/webgl_simple_math/src/main.rs
@@ -0,0 +1,19 @@
+
+extern crate webgl_simple_math;
+
+use webgl_simple_math::vector::vector_three::Vec3;
+
+fn main(){
+    let mut v = Vec3::new(1.0, 1.0, 1.0);
+    let l: f64 = v.length();
+    println!("initial: {} {} {}: {}", v.x, v.y, v.z, l);
+
+    let w: Vec3 = v.normalize();
+    let l: f64 = w.length();
+    println!("normalize: {} {} {}: {}", w.x, w.y, w.z, l);
+}
+
+
+
+
+
